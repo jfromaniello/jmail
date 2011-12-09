f8fe2300d79f7495b877f8f92a74a5086f05ab91
Jose F. Romaniello <jfromaniello@gmail.com>
2011-12-09 17:29:46 -0300

    added Send-Growl to the continuous.ps1 script

--- Tools/qunit-runner/continuous.ps1
+++ Tools/qunit-runner/continuous.ps1
@@ -1,20 +1,53 @@
-$m = Get-Module -List pswatch 
-if(!$m) {
-	Write-Host "Uh-Oh you don't have the pswatch cmdlet installed. Let me handle that for you."
-	iex ((new-object net.webclient).DownloadString("http://bit.ly/Install-PsWatch"))
-} 
+####SETUP
+if(!(Get-Module -List PsGet)) {
+    Write-Host "Uh-Oh you don't have the PsGet installed. Let me handle that for you."
+    (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+}
+Import-Module PsGet
 
+if(!(Get-Module -List pswatch)) {
+    Write-Host "Uh-Oh you don't have the pswatch cmdlet installed. Let me handle that for you."
+    Install-Module PsWatch
+} 
 Import-Module pswatch
+
+if(!(Get-Module -List Send-Growl)) {
+    Write-Host "Uh-Oh you don't have the Send-Growl cmdlet installed. Let me handle that for you."
+    Install-Module Send-Growl
+}
+
+Import-Module Send-Growl
+
+$IsGrowlInstalled = !(!(Get-GrowlPath))
+if(!$IsGrowlInstalled ) {
+    Write-Host "You don't have Growl installed... changing to Sad-Mode" -fore Red
+    Write-Host "If you wanna be cool install Growl For Windows from here http://www.growlforwindows.com" -fore Red
+}else{
+    Register-GrowlType jMail UnitTests
+    Send-Growl jMail UnitTests "Initialized" "Initialized with Happy-Mode=True" "http://be-happy.com"
+}
+##########
 
 $currentLocation = Get-Location
 
 watch "$currentLocation\..\..\" | %{
-	#Ignore $_
 	
-	$razorIndexTemplate = resolve-path .\..\..\index.cshtml
+    #Generate index.cshtml
+    $razorIndexTemplate = resolve-path .\..\..\index.cshtml
 	..\razorcandle\razorcandle.exe "$razorIndexTemplate"
 
+    #Generate all.tests.cshtml
 	$razorTemplate = resolve-path .\..\..\scripts\Tests\All.Tests.cshtml
 	..\razorcandle\razorcandle.exe "$razorTemplate" /M="{CI: false}"
-	.\phantomjs.exe run-qunit.js .\..\..\scripts\Tests\All.Tests.html
+
+    $allTestsRelativePath = ".\..\..\scripts\Tests\All.Tests.html" 
+	
+    .\phantomjs.exe run-qunit.js $allTestsRelativePath  |    
+            %{ 
+                if($IsGrowlInstalled  -and $_.Contains("passed,") -and $_.Contains("failed.")){
+                    $allTestsUrl = Resolve-Path $allTestsRelativePath
+                    Send-Growl jMail UnitTests "Tests Run" "$_" "file:///$allTestsUrl"
+                }
+                Write-Host $_
+            }
 }
